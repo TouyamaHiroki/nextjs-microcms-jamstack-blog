d07a7ce0bbb05033ad7840a29efc34bbf5730b7d
TouyamaHiroki <h-touyama@h-technology.jp>
2023-06-04 18:42:46 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { client } from '@/libs/client';
+
+import styles from '@/styles/Home.module.scss'
+import Link from 'next/link';
+
+// SSGを使ってデータを取得
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  console.log(data);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+  <div>
+    { blog.map((blog) => (
+      <li key={blog.id}>
+        <Link href={`blog/${blog.id}`}>
+          {blog.title}
+        </Link>
+      </li>
+    ))}
+  </div>
+  )
+};
